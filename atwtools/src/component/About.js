bd3f9b47bdd87c70b1a2133e56ee9f78fdaf88e7
Prasong <thevoidreaver13@gmail.com>
2023-10-31 18:27:29 +0700

    31-10-66

--- atwtools/src/component/About.js
+++ atwtools/src/component/About.js
@@ -12,20 +12,21 @@ function About() {
                             <img src="assets/img/about.jpg" class="img-fluid" alt="" />
                         </div>
                         <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right">
-                            <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
+                            <h3>ATW TOOLS (THAILAND) CO., LTD.</h3>
                             <p class="fst-italic">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                                magna aliqua.
+                                Our work encompasses the following scope.
                             </p>
                             <ul>
-                                <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                                <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-                                <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+                                <li><i class="bi bi-check-circle"></i> Importing and Distributing a Variety of Brands of Products.</li>
+                                <li><i class="bi bi-check-circle"></i> Providing Rental Services for All Types of Hydraulic Equipment.</li>
+                                <li><i class="bi bi-check-circle"></i> Providing Maintenance and Repair Services for All Types of Hydraulic Equipment.</li>
+                                <li><i class="bi bi-check-circle"></i> Providing Calibration Services for Oil, Flow Meter, and Hydraulic Torque Wrench.</li>
                             </ul>
-                            <p>
-                                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                culpa qui officia deserunt mollit anim id est laborum
+                            <p style={{ textIndent: '3em' }}>
+                                <strong>ATW Tools(Thailand) Co., Ltd.</strong>Having expanded the business to became sold
+                                distributor many range products such as hydraulic Tools, Cold and Hot High Pressure
+                                Cleaners, Petroleum Flow Meter and Rotary Vane Pumps, Lubricants and Greases System,
+                                in order to completed and covered the business in Thailand.
                             </p>
                         </div>
                     </div>
@@ -41,24 +42,24 @@ function About() {
 
                         <div class="col-lg-4" data-aos="fade-up">
                             <div class="box">
-                                <span>01</span>
-                                <h4>Lorem Ipsum</h4>
+                                <span>Sale</span>
+                                <h4>Import and Distribute</h4>
                                 <p>Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat</p>
                             </div>
                         </div>
 
                         <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="150">
                             <div class="box">
-                                <span>02</span>
-                                <h4>Repellat Nihil</h4>
+                                <span>Maintenance</span>
+                                <h4>Maintenance and Repair</h4>
                                 <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
                             </div>
                         </div>
 
                         <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="300">
                             <div class="box">
-                                <span>03</span>
-                                <h4> Ad ad velit qui</h4>
+                                <span>Assembly</span>
+                                <h4>Creation and Assembly</h4>
                                 <p>Molestiae officiis omnis illo asperiores. Aut doloribus vitae sunt debitis quo vel nam quis</p>
                             </div>
                         </div>
--- atwtools/src/component/OurWork.js
+++ atwtools/src/component/OurWork.js
@@ -10,16 +10,16 @@ function OurWork() {
                     <div class="section-title">
                         <span>Portfolio</span>
                         <h2>Portfolio</h2>
-                        <p>Sit sint consectetur velit quisquam cupiditate impedit suscipit alias</p>
+                        <p>Collection of Pictures of the Work we have done for Customers.</p>
                     </div>
 
                     <div class="row" data-aos="fade-up">
                         <div class="col-lg-12 d-flex justify-content-center">
                             <ul id="portfolio-flters">
-                                <li data-filter="*" class="filter-active">All</li>
-                                <li data-filter=".filter-app">App</li>
-                                <li data-filter=".filter-card">Card</li>
-                                <li data-filter=".filter-web">Web</li>
+                                <li data-filter="*" class="filter-active">all</li>
+                                <li data-filter=".filter-app">hydraulic</li>
+                                <li data-filter=".filter-card">oil equipment</li>
+                                <li data-filter=".filter-web">other tools</li>
                             </ul>
                         </div>
                     </div>
